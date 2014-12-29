93a85bb1945af6a32fe17a288c78723fa09cc62b
rodica-andronache <rodik_andronache@yahoo.com>
2014-12-29 16:09:16 +0200

    This fixes #23 search form

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" id="searchform" class="searchform" action="http://localhost/wordpress/">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div>
 		<input type="text" value="" name="s" id="s">
 		<input type="submit" id="searchsubmit" value="Search">
